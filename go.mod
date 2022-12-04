29a8901971698401defae950d18da8558b1d082a
Andrea Damiani <andrea_damiani@protonmail.com>
2022-12-04 18:37:55 +0100

    Add integration tests

--- go.mod
+++ go.mod
@@ -3,19 +3,30 @@ module github.com/damianiandrea/go-mongo-nats-connector
 go 1.17
 
 require (
+	github.com/nats-io/nats.go v1.19.0
+	github.com/stretchr/testify v1.8.1
+	go.mongodb.org/mongo-driver v1.10.1
+	golang.org/x/sync v0.0.0-20220819030929-7fc1605a5dde
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/klauspost/compress v1.15.11 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
-	github.com/nats-io/nats.go v1.16.0 // indirect
+	github.com/nats-io/nats-server/v2 v2.9.8 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.10.1 // indirect
-	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
-	golang.org/x/sync v0.0.0-20220819030929-7fc1605a5dde // indirect
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ test/integration/integration_test.go
@@ -0,0 +1,136 @@
+//go:build integration
+
+package integration
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/nats-io/nats.go"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	mongoUri             = os.Getenv("MONGO_URI")
+	natsUrl              = os.Getenv("NATS_URL")
+	reconnectionAttempts = os.Getenv("RECONNECTION_ATTEMPTS")
+	connectorHost        = os.Getenv("CONNECTOR_HOST")
+
+	mongoClient *mongo.Client
+
+	natsJs nats.JetStreamContext
+)
+
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
+	if err != nil {
+		log.Fatalf("could not connect to mongodb: %v", err)
+	}
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		log.Fatalf("could not ping mongodb: %v", err)
+	}
+	mongoClient = client
+
+	natsConn, err := nats.Connect(natsUrl)
+	if err != nil {
+		log.Fatalf("could not connect to nats: %v", err)
+	}
+	natsJs, err = natsConn.JetStream()
+	if err != nil {
+		log.Fatalf("could not create nats jetstream context: %v", err)
+	}
+
+	attemptsLeft, _ := strconv.ParseUint(reconnectionAttempts, 10, 64)
+	for attemptsLeft > 0 {
+		response, err := http.Get(fmt.Sprintf("%s/healthz", connectorHost))
+		if err == nil && response.StatusCode == http.StatusOK {
+			break
+		}
+		attemptsLeft--
+		log.Printf("connector not ready, reconnection attempts left: %d\n", attemptsLeft)
+		time.Sleep(1 * time.Second)
+	}
+	if attemptsLeft == 0 {
+		log.Fatalf("attempts exhausted: could not reach connector")
+	}
+
+	code := m.Run()
+
+	if err := client.Disconnect(ctx); err != nil {
+		log.Fatalf("could not close mongodb client: %v", err)
+	}
+	natsConn.Close()
+	os.Exit(code)
+}
+
+func TestWatchedCollectionsWereCreated(t *testing.T) {
+	testCollectionsWereCreated(t, "test-connector", "coll1", "coll2")
+}
+
+func TestResumeTokenCollectionsWereCreated(t *testing.T) {
+	testCollectionsWereCreated(t, "resume-tokens", "coll1", "coll2")
+}
+
+func testCollectionsWereCreated(t *testing.T, dbName string, collNames ...string) {
+	db := mongoClient.Database(dbName)
+	opts := options.ListCollections().SetNameOnly(true)
+	colls, err := db.ListCollectionNames(context.Background(), bson.D{}, opts)
+
+	require.NoError(t, err)
+	for _, collName := range collNames {
+		require.Contains(t, colls, collName)
+	}
+}
+
+func TestStreamsWereCreated(t *testing.T) {
+	streamsCh := natsJs.StreamNames()
+	streams := make([]string, 0)
+	for stream := range streamsCh {
+		streams = append(streams, stream)
+	}
+
+	require.Contains(t, streams, "COLL1")
+	require.Contains(t, streams, "COLL2")
+}
+
+func TestMongoInsertIsPublishedToNats(t *testing.T) {
+	db := mongoClient.Database("test-connector")
+	coll1 := db.Collection("coll1")
+
+	result, err := coll1.InsertOne(context.Background(), bson.D{{Key: "message", Value: "hi"}})
+	require.NoError(t, err)
+	require.NotNil(t, result.InsertedID)
+
+	sub, err := natsJs.SubscribeSync("COLL1.insert", nats.OrderedConsumer())
+	require.NoError(t, err)
+
+	msg, err := sub.NextMsg(1 * time.Minute)
+	require.NoError(t, err)
+	require.Equal(t, "COLL1.insert", msg.Subject)
+
+	expectedMsgData := &changeEvent{FullDocument{Message: "hi"}}
+	actualMsgData := &changeEvent{}
+	err = json.Unmarshal(msg.Data, actualMsgData)
+	require.NoError(t, err)
+	require.Equal(t, expectedMsgData, actualMsgData)
+}
+
+type changeEvent struct {
+	FullDocument `json:"fullDocument"`
+}
+
+type FullDocument struct {
+	Message string `json:"message"`
+}
