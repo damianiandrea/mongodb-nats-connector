b34636909e8b43120791a9ddb00deb1fd1ceafdd
Andrea Damiani <andrea_damiani@protonmail.com>
2023-01-02 16:37:26 +0100

    Wrap & name server test

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -16,47 +16,49 @@ import (
 )
 
 func TestServer(t *testing.T) {
-	addr := "127.0.0.1:8080"
-	ctx := context.Background()
-	cmpUp := &testComponent{name: "cmp_up", err: nil}
-	cmpDown := &testComponent{name: "cmp_down", err: errors.New("not pingable")}
+	t.Run("should create and run server and successfully call its health endpoint", func(t *testing.T) {
+		addr := "127.0.0.1:8080"
+		ctx := context.Background()
+		cmpUp := &testComponent{name: "cmp_up", err: nil}
+		cmpDown := &testComponent{name: "cmp_down", err: errors.New("not pingable")}
 
-	srv := New(
-		slog.Default(),
-		WithAddr(addr),
-		WithContext(ctx),
-		WithMonitoredComponents(cmpUp, cmpDown),
-	)
+		srv := New(
+			slog.Default(),
+			WithAddr(addr),
+			WithContext(ctx),
+			WithMonitoredComponents(cmpUp, cmpDown),
+		)
 
-	require.Equal(t, addr, srv.addr)
-	require.Equal(t, ctx, srv.ctx)
-	require.Contains(t, srv.components, cmpUp)
-	require.Contains(t, srv.components, cmpDown)
+		require.Equal(t, addr, srv.addr)
+		require.Equal(t, ctx, srv.ctx)
+		require.Contains(t, srv.components, cmpUp)
+		require.Contains(t, srv.components, cmpDown)
 
-	// start server
-	go start(srv)
+		// start server
+		go start(srv)
 
-	// stop server when done
-	defer stop(srv)
+		// stop server when done
+		defer stop(srv)
 
-	var res *http.Response
-	err := test.Await(5*time.Second, func() bool {
-		healthRes, err := healthcheck(srv)
-		res = healthRes
-		return err == nil
+		var res *http.Response
+		err := test.Await(5*time.Second, func() bool {
+			healthRes, err := healthcheck(srv)
+			res = healthRes
+			return err == nil
+		})
+		require.NoError(t, err)
+		gotBody := healthResponse{}
+		require.Equal(t, http.StatusOK, res.StatusCode)
+		require.Equal(t, "application/json", res.Header.Get("Content-Type"))
+		require.NoError(t, json.NewDecoder(res.Body).Decode(&gotBody))
+		require.Equal(t, healthResponse{
+			Status: UP,
+			Components: map[string]monitoredComponents{
+				"cmp_up":   {Status: UP},
+				"cmp_down": {Status: DOWN},
+			},
+		}, gotBody)
 	})
-	require.NoError(t, err)
-	gotBody := healthResponse{}
-	require.Equal(t, http.StatusOK, res.StatusCode)
-	require.Equal(t, "application/json", res.Header.Get("Content-Type"))
-	require.NoError(t, json.NewDecoder(res.Body).Decode(&gotBody))
-	require.Equal(t, healthResponse{
-		Status: UP,
-		Components: map[string]monitoredComponents{
-			"cmp_up":   {Status: UP},
-			"cmp_down": {Status: DOWN},
-		},
-	}, gotBody)
 }
 
 func start(srv *Server) {
