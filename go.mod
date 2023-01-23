5b625d28709bc7304ff81d5605281132f5b17be8
Andrea Damiani <andrea_damiani@protonmail.com>
2023-01-23 14:36:30 +0100

    Add nats client tests

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/damianiandrea/go-mongo-nats-connector
 go 1.19
 
 require (
+	github.com/nats-io/nats-server/v2 v2.9.8
 	github.com/nats-io/nats.go v1.19.0
 	github.com/stretchr/testify v1.8.1
 	go.mongodb.org/mongo-driver v1.10.1
@@ -16,8 +17,9 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.15.11 // indirect
+	github.com/minio/highwayhash v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
-	github.com/nats-io/nats-server/v2 v2.9.8 // indirect
+	github.com/nats-io/jwt/v2 v2.3.0 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -27,6 +29,8 @@ require (
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
+	golang.org/x/sys v0.1.0 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
---
+++ internal/nats/client_test.go
@@ -0,0 +1,181 @@
+package nats
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	natsserver "github.com/nats-io/nats-server/v2/server"
+	natstest "github.com/nats-io/nats-server/v2/test"
+	"github.com/nats-io/nats.go"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/exp/slog"
+)
+
+func TestNewClient(t *testing.T) {
+	t.Run("should create client with defaults", func(t *testing.T) {
+		s := natstest.RunDefaultServer()
+		defer s.Shutdown()
+		_ = s.EnableJetStream(&natsserver.JetStreamConfig{})
+
+		client, err := NewClient()
+
+		require.NoError(t, err)
+		require.NoError(t, err)
+		require.Equal(t, "nats://127.0.0.1:4222", client.url)
+		require.Equal(t, "nats", client.name)
+		require.Equal(t, slog.Default(), client.logger)
+		require.NotNil(t, client.conn)
+		require.NotNil(t, client.js)
+	})
+	t.Run("should create client with the configured options", func(t *testing.T) {
+		s := natstest.RunDefaultServer()
+		defer s.Shutdown()
+		_ = s.EnableJetStream(&natsserver.JetStreamConfig{})
+		url := nats.DefaultURL
+		logger := slog.New(slog.NewJSONHandler(os.Stdout))
+
+		client, err := NewClient(
+			WithNatsUrl(url),
+			WithLogger(logger),
+		)
+
+		require.NoError(t, err)
+		require.Equal(t, url, client.url)
+		require.Equal(t, "nats", client.name)
+		require.Equal(t, logger, client.logger)
+		require.NotNil(t, client.conn)
+		require.NotNil(t, client.js)
+	})
+	t.Run("should return error cause nats is not available", func(t *testing.T) {
+		client, err := NewClient()
+
+		require.Nil(t, client)
+		require.Error(t, err)
+	})
+}
+
+func TestClient_Name(t *testing.T) {
+	t.Run("should return client's name", func(t *testing.T) {
+		s := natstest.RunDefaultServer()
+		defer s.Shutdown()
+		_ = s.EnableJetStream(&natsserver.JetStreamConfig{})
+		client, _ := NewClient()
+
+		name := client.Name()
+
+		require.Equal(t, "nats", name)
+	})
+}
+
+func TestClient_Monitor(t *testing.T) {
+	t.Run("should return nil when client is connected", func(t *testing.T) {
+		s := natstest.RunDefaultServer()
+		defer s.Shutdown()
+		_ = s.EnableJetStream(&natsserver.JetStreamConfig{})
+		client, _ := NewClient()
+
+		err := client.Monitor(context.Background())
+
+		require.NoError(t, err)
+	})
+	t.Run("should return error when client is disconnected", func(t *testing.T) {
+		s := natstest.RunDefaultServer()
+		defer s.Shutdown()
+		_ = s.EnableJetStream(&natsserver.JetStreamConfig{})
+		client, _ := NewClient()
+		client.conn.Close()
+
+		err := client.Monitor(context.Background())
+
+		require.EqualError(t, err, ErrClientDisconnected.Error())
+	})
+}
+
+func TestClient_Close(t *testing.T) {
+	t.Run("should close client connection", func(t *testing.T) {
+		s := natstest.RunDefaultServer()
+		defer s.Shutdown()
+		_ = s.EnableJetStream(&natsserver.JetStreamConfig{})
+		client, _ := NewClient()
+
+		err := client.Close()
+
+		require.NoError(t, err)
+		require.True(t, client.conn.IsClosed())
+	})
+}
+
+func TestClient_AddStream(t *testing.T) {
+	t.Run("should add stream with the given name", func(t *testing.T) {
+		s := natstest.RunDefaultServer()
+		defer s.Shutdown()
+		_ = s.EnableJetStream(&natsserver.JetStreamConfig{})
+		client, _ := NewClient()
+
+		err := client.AddStream(context.Background(), &AddStreamOptions{StreamName: "TEST"})
+
+		require.NoError(t, err)
+		stream, err := client.js.StreamInfo("TEST")
+		require.NoError(t, err)
+		require.Equal(t, "TEST", stream.Config.Name)
+		require.Contains(t, stream.Config.Subjects, "TEST.*")
+		require.Equal(t, nats.FileStorage, stream.Config.Storage)
+	})
+	t.Run("should return error cause nats is not available", func(t *testing.T) {
+		s := natstest.RunDefaultServer()
+		defer s.Shutdown()
+		_ = s.EnableJetStream(&natsserver.JetStreamConfig{})
+		client, _ := NewClient()
+		client.conn.Close()
+
+		err := client.AddStream(context.Background(), &AddStreamOptions{StreamName: "TEST"})
+
+		require.Error(t, err)
+	})
+}
+
+func TestClient_Publish(t *testing.T) {
+	t.Run("should publish message based on the given options", func(t *testing.T) {
+		s := natstest.RunDefaultServer()
+		defer s.Shutdown()
+		_ = s.EnableJetStream(&natsserver.JetStreamConfig{})
+		client, _ := NewClient()
+		_, _ = client.js.AddStream(&nats.StreamConfig{
+			Name:     "TEST",
+			Subjects: []string{"TEST.*"},
+			Storage:  nats.FileStorage,
+		})
+
+		err := client.Publish(context.Background(), &PublishOptions{
+			Subj:  "TEST.insert",
+			MsgId: "123",
+			Data:  []byte("test"),
+		})
+
+		require.NoError(t, err)
+		sub, err := client.js.SubscribeSync("TEST.insert", nats.OrderedConsumer())
+		require.NoError(t, err)
+		msg, err := sub.NextMsg(5 * time.Second)
+		require.NoError(t, err)
+		require.Equal(t, "TEST.insert", msg.Subject)
+		require.Contains(t, msg.Header[nats.MsgIdHdr], "123")
+		require.Equal(t, []byte("test"), msg.Data)
+	})
+	t.Run("should return error cause nats is not available", func(t *testing.T) {
+		s := natstest.RunDefaultServer()
+		defer s.Shutdown()
+		_ = s.EnableJetStream(&natsserver.JetStreamConfig{})
+		client, _ := NewClient()
+		client.conn.Close()
+
+		err := client.Publish(context.Background(), &PublishOptions{
+			Subj:  "TEST.insert",
+			MsgId: "123",
+			Data:  []byte("test"),
+		})
+
+		require.Error(t, err)
+	})
+}
